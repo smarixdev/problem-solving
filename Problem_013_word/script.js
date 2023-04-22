22a87760fffaa37c95ac12b9c6af3fe36b627fc1
Ahmed M. Znaty <ahmdmznaty@gmail.com>
2023-04-22 18:15:39 +0200

    Problem { 013 } word

---
+++ Problem_013_word/script.js
@@ -0,0 +1,30 @@
+
+function word( str ) {
+    let nums = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+    ]
+    return nums.indexOf( str )
+    // let nums = {
+    //     zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9
+    // }
+    // return nums[str]
+    // if( str == "zero" ) { return 0 }
+    // else if( str == "one" ) { return 1 }
+    // else if( str == "two" ) { return 2 }
+    // else if( str == "three" ) { return 3 }
+    // else if( str == "four" ) { return 4 }
+    // else if( str == "five" ) { return 5 }
+    // else if( str == "six" ) { return 6 }
+    // else if( str == "seven" ) { return 7 }
+    // else if( str == "eight" ) { return 8 }
+    // else if( str == "nine" ) { return 9 }
+}
+
+console.log( word("one") )
+
+console.log( word("two") )
+
+console.log( word("nine") )
+
+
+
--- README.md
+++ README.md
@@ -134,3 +134,14 @@ You can find the playlist here: [**Problem Solving On Youtube**](https://www.you
 **See Code:** [**Code And Files**](https://github.com/webket/problem-solving/tree/main/Problem_012_areaShape)
 
 ---
+### Problem { 13 } word
+
+![The picture of the problem { 13 }](/images/Problem_013.png)
+
+**Watch Video:** [**On Youtube**](https://www.youtube.com/watch?v=wh_cZv6AvVc)
+
+**View Problem:** [**On Edabit**](https://edabit.com/challenge/ghkYG7dnSKd5NWoyt)
+
+**See Code:** [**Code And Files**](https://github.com/webket/problem-solving/tree/main/Problem_013_word)
+
+---
